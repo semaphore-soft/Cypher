48b73bd26854164b0ef382d7017a5273d720af9d
bagelhobbit <evanturner45@gmail.com>
2016-09-25 23:54:19 -0600

    Added error screen if wifi p2p is not supported.
    Application will attempt to turn wifi on if it is off, as wifi p2p needs wifi on to work (duh).

--- Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/WiFiDirectBroadcastReceiver.java
+++ Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/WiFiDirectBroadcastReceiver.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pInfo;
@@ -98,7 +99,7 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             try {
                 groupOwnerAddress = InetAddress.getByName(wifiP2pInfo.groupOwnerAddress.getHostAddress());
             } catch (UnknownHostException e) {
-                Log.e(TAG, "host IP: " + wifiP2pInfo.groupOwnerAddress.getHostAddress());
+                Log.e(TAG, "Host IP: " + wifiP2pInfo.groupOwnerAddress.getHostAddress());
                 e.printStackTrace();
             }
 
@@ -127,7 +128,30 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                 Log.d(TAG, "P2P enabled");
             } else {
                 // Wifi P2P is not enabled
-                Log.d(TAG, "P2P disabled, please fix");
+                Log.d(TAG, "P2P disabled");
+                // Check if wifi is disabled and enable it if so
+                WifiManager wifi = (WifiManager) mActivity.getSystemService(Context.WIFI_SERVICE);
+                if (!wifi.isWifiEnabled()) {
+                    wifi.setWifiEnabled(true);
+                    Toast.makeText(mActivity, "Enabled wifi", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Enabled wifi");
+                    return;
+                }
+
+                // Display an error if wifi P2P is not supported
+                AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+                builder.setTitle("Wifi P2P disabled");
+                builder.setMessage("This device does not support Wifi P2P.\nThe application will now exit.");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Close dialog first (avoids error on close), then exit activity
+                        dialogInterface.dismiss();
+                        mActivity.finish();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             // Request available peers. This is an asynchronous call. The calling activity
@@ -135,16 +159,16 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             if (mManager != null) {
                 mManager.requestPeers(mChannel, peerListListener);
             }
-            Log.d(TAG, "peer list changed");
+            Log.d(TAG, "Peer list changed");
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             // Respond to new connection or disconnections
-            Log.d(TAG, "connection changed");
+            Log.d(TAG, "Connection changed");
 
             if (mManager == null) {
                 return;
             }
 
-            NetworkInfo networkInfo = (NetworkInfo) intent.getParcelableExtra(
+            NetworkInfo networkInfo = intent.getParcelableExtra(
                     WifiP2pManager.EXTRA_NETWORK_INFO);
 
             if (networkInfo.isConnected()) {
@@ -155,7 +179,7 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
             // Respond to this device's wifi state changing
-            Log.d(TAG, "this device's state changed");
+            Log.d(TAG, "This device's state changed");
         }
     }
 }
