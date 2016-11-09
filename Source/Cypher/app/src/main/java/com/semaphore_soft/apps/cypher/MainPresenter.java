1586e1744bd5cd5b03754a706a62237691358d08
scorple <rick.matza@gmail.com>
2016-11-09 12:27:44 -0700

    added presenter class; added basic fragment logic; updated layout for programmatic fragment handling

---
+++ Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/MainPresenter.java
@@ -0,0 +1,21 @@
+package com.semaphore_soft.apps.cypher;
+
+/**
+ * Created by Scorple on 10/27/2016.
+ */
+class MainPresenter
+{
+    private MainActivity view;
+
+    MainPresenter()
+    {
+
+    }
+
+    void setView(MainActivity view)
+    {
+        this.view = view;
+        MainActivityFragment portal = new MainActivityFragment();
+        view.setPortal(portal, 0);
+    }
+}
