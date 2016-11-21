ea4604b4801f0f9d2a4a132dd7e4e7e7f2de5b5e
bagelhobbit <evanturner45@gmail.com>
2016-11-21 09:33:45 -0700

    Progressbar should work now

--- Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/MainActivity.java
+++ Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/MainActivity.java
@@ -1,6 +1,7 @@
 package com.semaphore_soft.apps.cypher;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
@@ -20,6 +21,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.util.HashMap;
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements WiFiServicesList.
     private IntentFilter mIntentFiler = new IntentFilter();
     private WifiP2pDnsSdServiceRequest serviceRequest;
 
-//    private ProgressDialog progress;
+    private ProgressDialog progress;
     private int hostWillingness;
     private final int SERVER_PORT = 58008;
     private final HashMap<String, String> buddies = new HashMap<>();
@@ -64,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements WiFiServicesList.
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
         mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-//        progress = new ProgressDialog(this);
+        progress = new ProgressDialog(this);
 
         mIntentFiler.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
         mIntentFiler.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
@@ -72,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements WiFiServicesList.
         mIntentFiler.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
         servicesList = new WiFiServicesList();
-        getFragmentManager().beginTransaction().add(R.id.fragment, servicesList, "services").commit();
+        getFragmentManager().beginTransaction().add(R.id.servicesRoot, servicesList, "services").commit();
 
         Button findGame = (Button) findViewById(R.id.connect);
         findGame.setOnClickListener(new View.OnClickListener() {
@@ -289,8 +291,8 @@ public class MainActivity extends AppCompatActivity implements WiFiServicesList.
                         service.device = device;
                         service.instanceName = instanceName;
                         service.serviceRegistrationType = registrationType;
-//                        ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
-//                        pb.setVisibility(View.INVISIBLE);
+                        ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
+                        pb.setVisibility(View.INVISIBLE);
                         adapter.add(service);
                         adapter.notifyDataSetChanged();
                         Log.d(TAG, "Service available " + instanceName);
@@ -339,8 +341,8 @@ public class MainActivity extends AppCompatActivity implements WiFiServicesList.
                     }
                 });
                 progress.show();*/
-//                ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
-//                pb.setVisibility(View.VISIBLE);
+                ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
+                pb.setVisibility(View.VISIBLE);
             }
 
             @Override
