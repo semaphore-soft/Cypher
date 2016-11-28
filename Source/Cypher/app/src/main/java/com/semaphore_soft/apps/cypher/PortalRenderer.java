d2c8864e75a550958b0917333aa904c827e82244
scorple <rick.matza@gmail.com>
2016-11-28 12:53:51 -0700

    added GLES20 shader classes; added demo triangle classes; updated renderer

--- Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/PortalRenderer.java
+++ Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/PortalRenderer.java
@@ -1,22 +1,32 @@
 package com.semaphore_soft.apps.cypher;
 
-import org.artoolkit.ar.base.ARToolKit;
-import org.artoolkit.ar.base.rendering.ARRenderer;
-import org.artoolkit.ar.base.rendering.Cube;
+import android.opengl.GLES20;
 
+import com.semaphore_soft.apps.cypher.shader.SimpleFragmentShader;
+import com.semaphore_soft.apps.cypher.shader.SimpleShaderProgram;
+import com.semaphore_soft.apps.cypher.shader.SimpleVertexShader;
+
+import org.artoolkit.ar.base.ARToolKit;
+import org.artoolkit.ar.base.rendering.gles20.ARRendererGLES20;
+import org.artoolkit.ar.base.rendering.gles20.CubeGLES20;
+import org.artoolkit.ar.base.rendering.gles20.ShaderProgram;
+
+import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Created by rickm on 11/9/2016.
  */
 
-class PortalRenderer extends ARRenderer {
+class PortalRenderer extends ARRendererGLES20
+{
 
     private int mark0 = -1;
     private int mark1 = -1;
     private int mark2 = -1;
 
-    private Cube    cube     = new Cube(40.0f, 0.0f, 0.0f, 20.0f);
+    private CubeGLES20       cube;
+    private ARTriangleGLES20 arTriangleGLES20;
 
     @Override
     public boolean configureARScene()
@@ -30,6 +40,24 @@ class PortalRenderer extends ARRenderer {
         }
 
         return true;
+    }
+
+    //Shader calls should be within a GL thread that is onSurfaceChanged(), onSurfaceCreated() or onDrawFrame()
+    //As the cube instantiates the shader during setShaderProgram call we need to create the cube here.
+    @Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config)
+    {
+        super.onSurfaceCreated(unused, config);
+
+        ShaderProgram cubeShaderProgram =
+            new SimpleShaderProgram(36, new SimpleVertexShader(), new SimpleFragmentShader());
+        cube = new CubeGLES20(40.0f, 0.0f, 0.0f, 20.0f);
+        cube.setShaderProgram(cubeShaderProgram);
+
+        ShaderProgram triangleShaderProgram =
+            new SimpleShaderProgram(3, new SimpleVertexShader(), new SimpleFragmentShader());
+        arTriangleGLES20 = new ARTriangleGLES20(40.0f, 0.0f, 0.0f, 20.0f);
+        arTriangleGLES20.setShaderProgram(triangleShaderProgram);
     }
 
     /**
@@ -54,13 +82,6 @@ class PortalRenderer extends ARRenderer {
                 ARToolKit.getInstance().queryMarkerVisible(mark1) &&
                 ARToolKit.getInstance().queryMarkerVisible(mark2))
         {
-
-            gl.glLoadMatrixf(ARToolKit.getInstance().queryMarkerTransformation(mark0), 0);
-
-            gl.glPushMatrix();
-            cube.draw(gl);
-            gl.glPopMatrix();
-
             float[] mark0TransInfo = ARToolKit.getInstance().queryMarkerTransformation(mark0);
             float[] mark0PosInfo =
                     {mark0TransInfo[mark0TransInfo.length - 4], mark0TransInfo[mark0TransInfo.length -
@@ -125,6 +146,96 @@ class PortalRenderer extends ARRenderer {
             gl.glPushMatrix();
             cube.draw(gl);
             gl.glPopMatrix();
+        }
+    }
+
+    /**
+     * Override the render function from {@link ARRendererGLES20}.
+     */
+    @Override
+    public void draw()
+    {
+        super.draw();
+
+        GLES20.glEnable(GLES20.GL_CULL_FACE);
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        GLES20.glFrontFace(GLES20.GL_CW);
+
+        float[] projectionMatrix = ARToolKit.getInstance().getProjectionMatrix();
+
+        // If the marker is visible, apply its transformation, and render a triangle
+        if (ARToolKit.getInstance().queryMarkerVisible(mark0))
+        {
+            arTriangleGLES20.draw(projectionMatrix,
+                                  ARToolKit.getInstance().queryMarkerTransformation(mark0));
+        }
+
+        if (ARToolKit.getInstance().queryMarkerVisible(mark0) &&
+            ARToolKit.getInstance().queryMarkerVisible(mark1) &&
+            ARToolKit.getInstance().queryMarkerVisible(mark2))
+        {
+            float[] mark0TransInfo = ARToolKit.getInstance().queryMarkerTransformation(mark0);
+            float[] mark0PosInfo =
+                {mark0TransInfo[mark0TransInfo.length - 4], mark0TransInfo[mark0TransInfo.length -
+                                                                           3], mark0TransInfo[
+                    mark0TransInfo.length - 2]};
+
+            System.out.println("begin mark0 pos matrix");
+            for (float x : mark0PosInfo)
+            {
+                System.out.println(x);
+            }
+            System.out.println("end mark0 pos matrix");
+
+            float[] mark1TransInfo = ARToolKit.getInstance().queryMarkerTransformation(mark1);
+            float[] mark1PosInfo =
+                {mark1TransInfo[mark1TransInfo.length - 4], mark1TransInfo[mark1TransInfo.length -
+                                                                           3], mark1TransInfo[
+                    mark1TransInfo.length - 2]};
+
+            System.out.println("begin mark1 pos matrix");
+            for (float x : mark1PosInfo)
+            {
+                System.out.println(x);
+            }
+            System.out.println("end mark1 pos matrix");
+
+            float[] mark2TransInfo = ARToolKit.getInstance().queryMarkerTransformation(mark2);
+            float[] mark2PosInfo =
+                {mark2TransInfo[mark2TransInfo.length - 4], mark2TransInfo[mark2TransInfo.length -
+                                                                           3], mark2TransInfo[
+                    mark2TransInfo.length - 2]};
+
+            System.out.println("begin mark2 pos matrix");
+            for (float x : mark2PosInfo)
+            {
+                System.out.println(x);
+            }
+            System.out.println("end mark2 pos matrix");
+
+            double distanceMark0Mark1 = Math.sqrt(Math.pow(mark0PosInfo[0] - mark1PosInfo[0], 2) +
+                                                  Math.pow(mark0PosInfo[1] - mark1PosInfo[1], 2) +
+                                                  Math.pow(mark0PosInfo[2] - mark1PosInfo[2], 2));
+
+            double distanceMark0Mark2 = Math.sqrt(Math.pow(mark0PosInfo[0] - mark2PosInfo[0], 2) +
+                                                  Math.pow(mark0PosInfo[1] - mark2PosInfo[1], 2) +
+                                                  Math.pow(mark0PosInfo[2] - mark2PosInfo[2], 2));
+
+            System.out.println("distance between mark0 and mark1 is " + distanceMark0Mark1);
+            System.out.println("distance between mark0 and mark2 is " + distanceMark0Mark2);
+
+            if (distanceMark0Mark1 < distanceMark0Mark2)
+            {
+                System.out.println("mark0 is closer to mark1");
+                cube.draw(projectionMatrix,
+                          ARToolKit.getInstance().queryMarkerTransformation(mark1));
+            }
+            else
+            {
+                System.out.println("mark0 is closer to mark2");
+                cube.draw(projectionMatrix,
+                          ARToolKit.getInstance().queryMarkerTransformation(mark2));
+            }
         }
     }
 }
