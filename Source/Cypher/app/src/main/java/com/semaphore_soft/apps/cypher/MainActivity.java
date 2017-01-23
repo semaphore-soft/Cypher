db4182a3d57cf353275243677f12f2bcb87365ce
bagelhobbit <evanturner45@gmail.com>
2017-01-23 15:43:10 -0700

    Added better logging for removing service requests

--- Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/MainActivity.java
+++ Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/MainActivity.java
@@ -225,7 +225,17 @@ public class MainActivity extends AppCompatActivity implements WiFiServicesList.
         config.groupOwnerIntent = hostWillingness;
         if (serviceRequest != null)
         {
-            mManager.removeServiceRequest(mChannel, serviceRequest, null);
+            mManager.removeServiceRequest(mChannel, serviceRequest, new WifiP2pManager.ActionListener() {
+                @Override
+                public void onSuccess() {
+                    Log.d(TAG, "Removed service request");
+                }
+
+                @Override
+                public void onFailure(int reason) {
+                    Log.d(TAG, "Failed to remove service request");
+                }
+            });
             mManager.connect(mChannel, config, new WifiP2pManager.ActionListener()
             {
                 @Override
@@ -361,6 +371,7 @@ public class MainActivity extends AppCompatActivity implements WiFiServicesList.
             @Override
             public void onSuccess()
             {
+                Log.d(TAG, "Removed service request");
                 mManager.addServiceRequest(mChannel, serviceRequest, new WifiP2pManager.ActionListener()
                 {
                     @Override
