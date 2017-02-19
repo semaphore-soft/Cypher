ee174bba38bc9bde3828ede7dc0821e0c5094401
bagelhobbit <evanturner45@gmail.com>
2017-02-19 13:43:27 -0700

    Removed menu from connection lobby

--- Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/ConnectionLobbyActivity.java
+++ Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/ConnectionLobbyActivity.java
@@ -14,8 +14,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -320,32 +318,4 @@ public class ConnectionLobbyActivity extends AppCompatActivity implements Respon
             mClientBound = false;
         }
     };
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_reconnect, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.action_reconnect)
-        {
-            if (host)
-            {
-                serverService.reconnect();
-            }
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
