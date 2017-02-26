9322e1d5651fbf7144b857a17065629204cff62a
bagelhobbit <evanturner45@gmail.com>
2017-02-26 16:21:02 -0700

    Made Room enum public again

--- Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/game/Room.java
+++ Source/Cypher/app/src/main/java/com/semaphore_soft/apps/cypher/game/Room.java
@@ -23,7 +23,7 @@ public class Room
      * Describes the type of a wall, including whether or not it hosts a door,
      * and, if there is a door, the status of that door.
      */
-    enum E_WALL_TYPE
+    public enum E_WALL_TYPE
     {
         NO_DOOR,
         DOOR_UNLOCKED,
